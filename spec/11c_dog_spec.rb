beb2ebac2bfb0e24f6edbe303c65b19c4469134f
Justin <173909633+DontEatMyPopcorn@users.noreply.github.com>
2026-07-18 01:43:40 -0400

    Complete 11.

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:pup) { described_class.new('Pup') }
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when dog is a class of Pet' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(pup.barking?).not_to be true
   end
 
   # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(pup.sleeping?).to be true
   end
 end
